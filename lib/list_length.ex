8df90427f3ee74b3f06807606614bf78d4202229
Lucas <lucassouza876@outlook.com>
2021-12-25 15:55:56 -0300

    Challenge finished

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,16 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: count(list, 0)
+
+  defp count([], acc), do: acc
+
+  defp count([_ | tail], acc) do
+    acc = acc + 1;
+    count(tail, acc)
   end
 end
+
+# 1, 2
+
+# 1 ex: [1, 2] hd: 1, tail [2], 0 -> acc = 0 + 1, count([2], 1)
+# 2 ex: [2] hd: 2, tail: [], 1 -> acc = 1 + 1, count([], 2)
+# 3 ex: [] acc = 2
